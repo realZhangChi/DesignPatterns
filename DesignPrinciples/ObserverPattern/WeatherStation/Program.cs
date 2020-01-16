e7e788ede6b108349603d4e1b13b8de635d95eb4
Chi <zliming@outlook.com>
2020-01-16 14:20:15 +0800

    :alembic: test

--- DesignPrinciples/ObserverPattern/WeatherStation/Program.cs
+++ DesignPrinciples/ObserverPattern/WeatherStation/Program.cs
@@ -6,7 +6,12 @@ namespace WeatherStation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var weatherData = new WeatherData();
+            var currentDisplay = new CurrentConditionsDisplay(weatherData);
+
+            weatherData.SetMeasurements(80, 65, 30.4f);
+            weatherData.SetMeasurements(82, 70, 29.2f);
+            weatherData.SetMeasurements(78, 90, 29.2f);
         }
     }
 }
